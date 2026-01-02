0a15d2533f7d015377bc6fe955769ce06a3d89f3
Кирилл <131317714+Morkamo@users.noreply.github.com>
2026-01-02 18:04:32 +0900

    Initial commit

---
+++ SCP-035/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Exiled.API.Features;
+using HarmonyLib;
+
+namespace SCP_035
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "SCP-035";
+        public override string Prefix => Name;
+        public override string Author => "Morkamo";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 12, 1);
+
+        public static Plugin Instance { get; private set; }
+        
+        public override void OnEnabled()
+        {
+            Instance = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
